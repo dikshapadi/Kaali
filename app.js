cd6dfbbdbef0b4a2d474d234b24287407918e93e
dikshapadi <dikshapadi@gmail.com>
2022-12-16 17:22:25 +0530

    h

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     res.locals.error = req.flash("error");
 	res.locals.success = req.flash("success");
 	res.locals.warning = req.flash("warning");
-	next();
+	next()
 });
 
 //Routes
