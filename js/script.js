06f3279439636783e3de74b53bd5c4b3bc7a0dfe
dikshapadi <dikshapadi@gmail.com>
2022-12-16 17:19:11 +0530

    Changes to be committed: 	renamed:    css/aboutUs.css -> assets/css/aboutUs.css 	renamed:    css/alertswrapper.css -> assets/css/alertswrapper.css 	renamed:    css/blog.css -> assets/css/blog.css 	renamed:    css/cheque.css -> assets/css/cheque.css 	renamed:    css/contact.css -> assets/css/contact.css 	renamed:    css/donation.css -> assets/css/donation.css 	renamed:    css/gallery.css -> assets/css/gallery.css 	renamed:    css/home.css -> assets/css/home.css 	renamed:    css/mainStat.css -> assets/css/mainStat.css 	renamed:    css/maindropDown.css -> assets/css/maindropDown.css 	renamed:    css/offline.css -> assets/css/offline.css 	renamed:    css/register.css -> assets/css/register.css 	renamed:    css/trans.css -> assets/css/trans.css 	renamed:    css/transition.css -> assets/css/transition.css 	renamed:    css/volunteer.css -> assets/css/volunteer.css 	renamed:    images/DSCE final logo.png -> assets/images/DSCE final logo.png 	renamed:    images/GBV.jpg -> assets/images/GBV.jpg 	renamed:    images/aqdas-modified.png -> assets/images/aqdas-modified.png 	renamed:    images/arrow.png -> assets/images/arrow.png 	renamed:    images/banner.png -> assets/images/banner.png 	renamed:    images/beyonce.jpg -> assets/images/beyonce.jpg 	renamed:    images/bg5.jpg -> assets/images/bg5.jpg 	renamed:    images/bgimage.jpeg -> assets/images/bgimage.jpeg 	renamed:    images/blog1.jpg -> assets/images/blog1.jpg 	renamed:    images/blog2.jpg -> assets/images/blog2.jpg 	renamed:    images/blog3.jpg -> assets/images/blog3.jpg 	renamed:    images/blog4.jpg -> assets/images/blog4.jpg 	renamed:    images/bottom1.png -> assets/images/bottom1.png 	renamed:    images/bottom2.png -> assets/images/bottom2.png 	renamed:    images/cash.jpg -> assets/images/cash.jpg 	renamed:    images/cheque.jpg -> assets/images/cheque.jpg 	renamed:    images/contact us.png -> assets/images/contact us.png 	renamed:    images/d.png -> assets/images/d.png 	renamed:    images/deon-modified.jpg -> assets/images/deon-modified.jpg 	renamed:    images/deon-modified.png -> assets/images/deon-modified.png 	renamed:    images/donatecoin.png -> assets/images/donatecoin.png 	renamed:    images/donation.webp -> assets/images/donation.webp 	renamed:    images/download (1).jpeg -> assets/images/download (1).jpeg 	renamed:    images/download.jpeg -> assets/images/download.jpeg 	renamed:    images/dsce1.jpg -> assets/images/dsce1.jpg 	renamed:    images/dsce2.jpg -> assets/images/dsce2.jpg 	renamed:    images/durga.png -> assets/images/durga.png 	renamed:    images/extra.jpg -> assets/images/extra.jpg 	renamed:    images/eyes.jpg -> assets/images/eyes.jpg 	renamed:    images/finalheader copy 2.png -> assets/images/finalheader copy 2.png 	renamed:    images/finalheader copy 3.png -> assets/images/finalheader copy 3.png 	renamed:    images/finalheader copy.png -> assets/images/finalheader copy.png 	renamed:    images/finalheader.png -> assets/images/finalheader.png 	renamed:    images/finalheader100.png -> assets/images/finalheader100.png 	renamed:    images/gallery.jpg -> assets/images/gallery.jpg 	renamed:    images/header.jpg -> assets/images/header.jpg 	renamed:    images/header4.png -> assets/images/header4.png renamed:    images/images (1).jpeg -> assets/images/images (1).jpeg 	renamed:    images/images.jpeg -> assets/images/images.jpeg 	renamed:    images/ind.jpg -> assets/images/ind.jpg 	renamed:    images/kaalilogo.jpg -> assets/images/kaalilogo.jpg 	renamed:    images/logo copy 2.png -> assets/images/logo copy 2.png 	renamed:    images/logo copy 3.png -> assets/images/logo copy 3.png 	renamed:    images/logo copy.png -> assets/images/logo copy.png 	renamed:    images/logo.png -> assets/images/logo.png 	renamed:    images/mill.jpg -> assets/images/mill.jpg 	renamed:    images/nalsa.png -> assets/images/nalsa.png 	renamed:    images/ncw.png -> assets/images/ncw.png

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const path = window.location.pathname;
-
-window.setTimeout(() => {
-	let alertsWrapper = document.querySelector(".alerts-wrapper");
-	if(alertsWrapper) {alertsWrapper.style.display = "none";}
-}, 3000);
-
